2bb50ce691e407482ffc86d5a81c81c67ce1ba42
zhangsr16 <145007394+zhangsr16@users.noreply.github.com>
2025-03-18 17:57:22 +0800

    Update cleanETF.R

--- cleanETF.R
+++ cleanETF.R
@@ -10,14 +10,13 @@ for (i in 1:len) {
   data=read.xlsx(list_name[i], startRow = 1, sheet=1)
   #clean
   data[,"现手"]=as.numeric(str_replace_all(data[,"现手"], "[↑↓-]", ""))
-  data[,"涨幅"]=as.numeric(str_replace_all(data[,"涨幅"], "--", "0"))
-  data[,"涨跌"]=as.numeric(str_replace_all(data[,"涨跌"], "--", "0"))  
-  data[,"总手"]=as.numeric(str_replace_all(data[,"总手"], "--", "0"))  
-  data[,"卖价"]=as.numeric(str_replace_all(data[,"卖价"], "--", "0"))  
+  for (j in 3:ncol(data)) {
+    data[, j]=as.numeric(str_replace_all(data[, j], "--", "0"))
+  }
   data=data[,-c(13:18,30)]
   data <- data[order(data$代码), ]
   #output
-  write_xlsx(data, path=str_c("./ETF/2025/", list_name[i]))
+  write_xlsx(data, path=str_c("./", str_replace(list_name[i], "-2ETF", "")))
 }
 
 
