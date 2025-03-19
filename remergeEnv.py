39dae3ab0c3ff5ad91fe88b58109d243e5d670de
zhangsr16 <145007394+zhangsr16@users.noreply.github.com>
2025-03-19 09:44:04 +0800

    Update remergeEnv.py

--- remergeEnv.py
+++ remergeEnv.py
@@ -12,23 +12,23 @@ nowSummary = [0, 1, 8]
 # sumETF = [0, 2, 6, 9, 10, 17, 18, 20, 21, 22]
 # nowETF = [1, 7, 8, 11Fst, 12Fst, 13MAX, 14MIN]
 
-def mergeState(statewindow, state, Option):
-    if Option == 'Area':
-        pass
-    elif Option == 'Field':
-        pass
-    elif Option == 'Summary':
-        sumcols = statewindow.columns[sumSummary]
-        avgcols = statewindow.columns[avgSummary]
-        statewindow[sumcols + avgcols] += state[sumcols + avgcols]
-        statewindow[avgcols] /= WindowPoint['Days']
-        nowcols = statewindow.columns[nowSummary]
-        statewindow[nowcols] = state[nowcols]
-        if WindowPoint['Days']==0:
-            statewindow[Fst] = state[Fst]
-        statewindow[Max] = max(state[Max], statewindow[Max])
-        statewindow[Min] = min(state[Min], statewindow[Min])
-    elif Option == 'Type':
-        pass
-    elif Option == 'ETF':
-        pass
+import pandas as pd
+
+# 示例数据
+data1 = {
+    'Max': [1, 3, 5, 7, 9],
+    'OtherColumn': [10, 20, 30, 40, 50]
+}
+
+data2 = {
+    'Max': [2, 2, 6, 6, 8],
+    'OtherColumn': [15, 25, 35, 45, 55]
+}
+
+# 创建两个 DataFrame
+state = pd.DataFrame(data1)
+statewindow = pd.DataFrame(data2)
+
+# 逐行获取 'Max' 列的最大值
+statewindow['Max'] = statewindow[['Max']].combine(state[['Max']], func=lambda s1, s2: s1.combine(s2, max))
+statewindow['OtherColumn'] = statewindow[['Max']].combine(state[['Max']], func=lambda s1, s2: s1.combine(s2, min))
